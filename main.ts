3f88273f233af8b5a435ca7c320375745c867923
Yaafy <132677932+Yaafy@users.noreply.github.com>
2023-05-05 17:13:04 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     led.toggle(0, 2)
 })
+input.onButtonPressed(Button.AB, function () {
+    led.toggle(0, 2)
+    led.toggle(4, 2)
+})
 input.onButtonPressed(Button.B, function () {
     led.toggle(4, 2)
 })
